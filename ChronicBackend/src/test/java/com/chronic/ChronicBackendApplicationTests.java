00a5b6e243b085fdcb611390085bc0e8826d2d73
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-27 11:53:56 +0530

    First

---
+++ ChronicBackend/src/test/java/com/chronic/ChronicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
