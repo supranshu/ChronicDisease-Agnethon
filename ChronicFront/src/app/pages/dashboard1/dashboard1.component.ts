5c28261b0f8a6aae2be27c901be8fc0ec81bd856
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-27 19:43:25 +0530

    DAsh1 Api Done

--- ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
+++ ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import baseUrl from '../../services/helper';
+import { error } from 'console';
 
 @Component({
   selector: 'app-dashboard1',
@@ -6,6 +11,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard1.component.css']
 })
 export class Dashboard1Component {
+
+  
   selectedDisease: string = 'diabetes';
   sugarLevel: string = '';
   hba1c: string = '';
@@ -13,18 +20,71 @@ export class Dashboard1Component {
   bloodPressure: string = '';
   weight: string = '';
   height: string = '';
+  bmi:number= this.bmiGet();
+  public bmiGet(){
+    const weightInKg = parseFloat(this.weight);
+    const heightInCm = parseFloat(this.height);
+    const heightInM = heightInCm / 100; 
+    return weightInKg / (heightInM * heightInM);
+  }
 
-  constructor() { }
+  localMail:any=localStorage.getItem("userData");
+  localMail1=this.localMail.email;
+
+
+  public diseaseData={
+    email:this.localMail1,
+    disease:this.selectedDisease,
+    height:this.height,
+    weight:this.weight,
+    bloodPressure:this.bloodPressure,
+    sugar:this.sugarLevel,
+    hba1c:this.hba1c,
+    bmi:this.bmi,
+    pefr:this.pefr
+
+  }
+
+  constructor(private http:HttpClient, private snack:MatSnackBar, private router:Router) { }
 
   submitForm(): void {
-    // Use the form values here for Gemini API call or any other operation
-    console.log('Form submitted');
-    console.log('Selected Disease:', this.selectedDisease);
-    console.log('Sugar Level:', this.sugarLevel);
-    console.log('HbA1c:', this.hba1c);
-    console.log('PEFR:', this.pefr);
-    console.log('Blood Pressure:', this.bloodPressure);
-    console.log('Weight:', this.weight);
-    console.log('Height:', this.height);
+
+    this.http.post(`${baseUrl}/disease`,this.diseaseData).subscribe(
+      (response)=>{
+        console.log(response)
+        this.snack.open("Disease Info updated!!!",'', {duration:3000})
+      },
+      (error)=>{
+        console.log(error)
+        this.snack.open("Something Went Wrong!!!",'', {duration:3000})
+      }
+    )
+    
+    // Determine which disease is selected and save relevant data in localStorage
+    switch (this.selectedDisease) {
+      case 'diabetes':
+        localStorage.setItem('diabetesSugarLevel', this.sugarLevel);
+        localStorage.setItem('diabetesHba1c', this.hba1c);
+        localStorage.setItem('weight', this.weight);
+        localStorage.setItem('height', this.height);
+        break;
+      case 'asthma':
+        localStorage.setItem('asthmaPefr', this.pefr);
+        localStorage.setItem('weight', this.weight);
+        localStorage.setItem('height', this.height);
+        break;
+      case 'hypertension':
+        localStorage.setItem('hypertensionBloodPressure', this.bloodPressure);
+        localStorage.setItem('weight', this.weight);
+        localStorage.setItem('height', this.height);
+        break;
+      default:
+        break;
+
+
+    }
+    
+
+    
   }
 }
