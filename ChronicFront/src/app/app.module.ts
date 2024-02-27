001c5960fa33ec1f76ec106cee0329ffcafbedb1
Mushtaq-tech <mohdmush2@gmail.com>
2024-02-27 19:44:28 +0530

    condition update

--- ChronicFront/src/app/app.module.ts
+++ ChronicFront/src/app/app.module.ts
@@ -1,33 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { KnowMoreComponent } from './pages/know-more/know-more.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FormsModule } from '@angular/forms';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { LoginComponent } from './pages/login/login.component';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { Dashboard1Component } from './pages/dashboard1/dashboard1.component';
 import { Dashboard2Component } from './pages/dashboard2/dashboard2.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
-
-
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-
     SignUpComponent,
     KnowMoreComponent,
     LoginComponent,
@@ -38,12 +29,12 @@ import { ContactComponent } from './pages/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, FormsModule, MatSnackBarModule, HttpClientModule
+    AppRoutingModule,
+    FormsModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
+++ ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-dashboard1',
+  templateUrl: './dashboard1.component.html', // Correct file name and path
+  styleUrls: ['./dashboard1.component.css']
 })
-export class LoginComponent {
 
-  
+export class Dashboard1Component {
+  selectedCondition: string = '';
+  additionalInfo: any = {}; // Customize types later
+
+  onSubmit(form: any) {
+    // Access form values and additionalInfo properties
+    const email = form.value.email;
+    const password = form.value.password;
+    const healthCondition = this.selectedCondition;
+    const additionalData = this.additionalInfo;
+
+    // Process the data (e.g., validation, store in service, send to server)
+    console.log(email, password, healthCondition, additionalData);
+
+    // Reset form and additional information
+    form.reset();
+    this.selectedCondition = '';
+    this.additionalInfo = {};
+  }
 }
