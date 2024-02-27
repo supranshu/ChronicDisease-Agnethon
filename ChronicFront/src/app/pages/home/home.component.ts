784d915a7e3e8f0d79358ced5fe8580926cfb5d7
Kunal Pandhare <kunalpn7@gmail.com>
2024-02-27 15:55:49 +0530

    styling

--- ChronicFront/src/app/pages/home/home.component.ts
+++ ChronicFront/src/app/pages/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  ngOnInit() {
+    this.animateCounters();
+  }
 
+  animateCounters() {
+    const counters = document.querySelectorAll<HTMLDivElement>(".count");
+    const speed = 200;
+
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = parseInt(counter.getAttribute("data-target") || "0", 10);
+        const count = parseInt(counter.innerText, 10);
+        const increment = Math.trunc(target / speed);
+
+        if (count < target) {
+          counter.innerText = (count + increment).toString();
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target.toString();
+        }
+      };
+      updateCount();
+    });
+  }
 }
