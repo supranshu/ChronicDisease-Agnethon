4f48e34aa87ba60d952e84f710c6f6e04e7cec88
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-27 19:01:01 +0530

    DAsh1

--- ChronicFront/src/app/app.module.ts
+++ ChronicFront/src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { KnowMoreComponent } from './pages/know-more/know-more.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FormsModule } from '@angular/forms';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { LoginComponent } from './pages/login/login.component';
-<<<<<<< HEAD
 import { Dashboard1Component } from './pages/dashboard1/dashboard1.component';
-=======
-import { AboutusComponent } from './aboutus/aboutus.component';
->>>>>>> 20d302d4abebb844bdfc45a8682ffbc8764a5e76
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
+ 
 
 @NgModule({
   declarations: [
@@ -28,11 +23,8 @@ import { AboutusComponent } from './aboutus/aboutus.component';
     SignUpComponent,
     KnowMoreComponent,
     LoginComponent,
-<<<<<<< HEAD
-    Dashboard1Component
-=======
+    Dashboard1Component,
     AboutusComponent
->>>>>>> 20d302d4abebb844bdfc45a8682ffbc8764a5e76
   ],
   imports: [
     BrowserModule,
--- ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
+++ ChronicFront/src/app/pages/dashboard1/dashboard1.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard1',
   templateUrl: './dashboard1.component.html',
-  styleUrl: './dashboard1.component.css'
+  styleUrls: ['./dashboard1.component.css']
 })
 export class Dashboard1Component {
+  selectedDisease: string = 'diabetes';
+  sugarLevel: string = '';
+  hba1c: string = '';
+  pefr: string = ''; 
+  bloodPressure: string = '';
+  weight: string = '';
+  height: string = '';
 
+  constructor() { }
+
+  submitForm(): void {
+    // Use the form values here for Gemini API call or any other operation
+    console.log('Form submitted');
+    console.log('Selected Disease:', this.selectedDisease);
+    console.log('Sugar Level:', this.sugarLevel);
+    console.log('HbA1c:', this.hba1c);
+    console.log('PEFR:', this.pefr);
+    console.log('Blood Pressure:', this.bloodPressure);
+    console.log('Weight:', this.weight);
+    console.log('Height:', this.height);
+  }
 }
