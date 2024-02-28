81cf6bb2e74e6aba9daf43a0851a65c31f5a8971
Kunal Pandhare <kunalpn7@gmail.com>
2024-02-28 08:36:37 +0530

    docAppointment

--- ChronicFront/src/app/pages/docappointment/docappointment.component.ts
+++ ChronicFront/src/app/pages/docappointment/docappointment.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import baseUrl from '../../services/helper';
+import { HttpClient } from '@angular/common/http';
+interface Doctor {
+  docName: string;
+  speciality: string;
+  city: string;
+  state: string;
+  country: string;
+  phone: string;
+  email: string;
+ 
+}
 @Component({
   selector: 'app-docappointment',
   templateUrl: './docappointment.component.html',
   styleUrl: './docappointment.component.css'
 })
 export class DocappointmentComponent {
+  docName = localStorage.getItem('docName');
+  email = localStorage.getItem('email');
+  doctors: Doctor[] = [];
 
+  constructor(private http: HttpClient) {}
+  fetchDoctors(city: string, state: string, country: string): void 
+{
+  
+  this.http.get<Doctor[]>(`${baseUrl}/all-doctors`)
+    .subscribe(
+      (response) => {
+        console.log(response);
+        this.doctors = response;
+        
+      },
+      (error: any) => {
+        console.error('Error fetching  doctors:', error);
+        
+        
+      }
+    );
+}
+  fetchDName(docName: string): void {
+  
+    this.http.get<Doctor[]>(`${baseUrl}/all-doctors/${this.email}`)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.doctors = response;
+          
+        },
+        (error: any) => {
+          console.error('Error fetching  doctors:', error);
+          
+          
+        }
+      );
+      }
 }
